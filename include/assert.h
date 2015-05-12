1fa1ffb292a0f591366ee2c426cedf532d7d53b4
Philippe Daouadi <p.daouadi@free.fr>
2015-05-12 22:33:11 +0200

    Implement assert

--- include/assert.h
+++ include/assert.h
@@ -1,6 +1,25 @@
 #ifndef ASSERT_H
 #define ASSERT_H
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifndef NDEBUG
+#define assert(expr) \
+  do { \
+    if (!(expr)) \
+      assert_fail(__FILE__, __LINE__, #expr); \
+  } while (0)
+#else
 #define assert(expr)
+#endif
+
+void assert_fail(const char* file, int line, const char* condition);
+
+#ifdef __cplusplus
+}
+#endif
+
 
 #endif
